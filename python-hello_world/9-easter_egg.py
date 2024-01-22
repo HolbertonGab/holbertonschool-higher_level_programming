72297842b6179b912c1e04ff59974354915664a3
HolbertonGab <7290@holbertonstudents.com>
2024-01-22 14:31:34 +0000

    fixing the code for this funny easter egg

--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,23 +1,2 @@
 #!/usr/bin/python3
-a = " is better than "
-b = "If the implementation is hard to explain, it"
-print(f"The Zen of Python, by Tim Peters\n\n\
-Beautiful{a}ugly.\n\
-Explicit{a}implicit.\n\
-Simple{a}complex.\n\
-Complex{a}complicated.\n\
-Flat{a}nested.\n\
-Sparse{a}dense.\n\
-Readability counts.\n\
-Special cases aren't special enough to break the rules.\n\
-Although practicality beats purity.\n\
-Errors should never pass silently.\n\
-Unless explicitly silenced.\n\
-In the face of ambiguity, refuse the temptation to guess.\n\
-There should be one-- and preferably only one --obvious way to do it.\n\
-Although that way may not be obvious at first unless you're Dutch.\n\
-Now{a}never.\n\
-Although never is often better than *right* now.\n\
-{b}'s a bad idea.\n\
-{b} may be a good idea.\n\
-Namespaces are one honking great idea -- let's do more of those!")
+import this
