bebf8331b08121dfd6d70f3d09650b4a58dcb09e
HolbertonGab <7290@holbertonstudents.com>
2024-01-24 12:48:27 +0000

    back to previous

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 def uppercase(str):
     for index in str:
-        index = ord(index)
-        if index >= 97 and index <= 122:
-            index -= 32
-        print("{}".format(chr(index)), end="")
+        if "a" <= index <= "z":
+            upper_char = chr(ord(index) - 32)
+        else:
+            upper_char = index
+        print("{}".format(upper_char), end="")
     print("")
