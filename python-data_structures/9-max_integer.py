9a5350799ffb77e48568c684e21b472ab59089ca
HolbertonGab <7290@holbertonstudents.com>
2024-01-30 13:58:08 +0000

    adding else condition

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,9 +2,9 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         max_val = None
-
-    max_val = my_list[0]
-    for num in my_list[1:]:
-        if num > max_val:
-            max_val = num
+    else:
+        max_val = my_list[0]
+        for num in my_list[1:]:
+            if num > max_val:
+                max_val = num
     return max_val
