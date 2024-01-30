8d7879fbf64026312cc44408f8982476ee11fb60
HolbertonGab <7290@holbertonstudents.com>
2024-01-30 10:29:22 +0000

    None added to return functions

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,8 +2,8 @@
 def element_at(my_list, idx):
     for idx in my_list:
         if idx < 0:
-            return
+            return None
         if idx > len(my_list):
-            return
+            return None
         else:
             return my_list[idx]
