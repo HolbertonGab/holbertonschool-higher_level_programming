ac43e58fb0d7d063b5000f5a4d9b1312363a90b1
HolbertonGab <7290@holbertonstudents.com>
2024-02-03 15:59:48 +0000

    changing except conditions

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,26 +1,20 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    result = []
-    for i in range(list_length):
+    if list_length == 0 or (not my_list_1 and not my_list_2):
+        return
+    new_list = [0] * list_length
+    for index in range(list_length):
         try:
-            value_1 = my_list_1[i]
-        except IndexError:
-            print("out of range")
-            value_1 = 0
-
-        try:
-            value_2 = my_list_2[i]
-        except IndexError:
-            print("out of range")
-            value_2 = 0
-
-        if not (isinstance(value_1, (int, float)) and isinstance(value_2, (int, float))):
+            new_list[index] = round(my_list_1[index] / my_list_2[index], 1)
+        except TypeError:
             print("wrong type")
-            result.append(0)
-        elif value_2 == 0:
+            new_list[index] = 0
+        except ZeroDivisionError:
             print("division by 0")
-            result.append(0)
-        else:
-            result.append(value_1 / value_2)
-
-    return result
+            new_list[index] = 0
+        except IndexError:
+            print("out of range")
+            new_list[index] = 0
+        finally:
+            continue
+    return new_list
