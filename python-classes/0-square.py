3d04fc86646f9172f25c4255fda8bb5857b90832
HolbertonGab <7290@holbertonstudents.com>
2024-02-05 12:45:22 +0000

    comments line added

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""Empty class Square that defines a square"""
+
+
 class Square:
-    """empty class Square that defines a square"""
+    """A class square"""
     def __init__(self,):
         ""
