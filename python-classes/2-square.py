da6f6fb21d4e83bd3679d58713292e07f339d082
HolbertonGab <7290@holbertonstudents.com>
2024-02-05 14:16:29 +0000

    updates on docstring

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-"""class Square that defines a square by: (based on 1-square.py)"""
+"""This module defines a class named Square.
+
+"""
 
 
 class Square:
-    """
-Private instance attribute: size
-Instantiation with optional size: def __init__(self, size=0):
-size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
-if size is less than 0, raise a ValueError exception with the message size must be >= 0
+    """class Square that defines a square by: (based on 1-square.py)
+
     """
     def __init__(self, size=0):
+        """Private instance attribute: size
+        Instantiation with optional size: def __init__(self, size=0):
+        size must be an integer, otherwise raise a TypeError exception
+        with the message size must be an integer
+        if size is less than 0, raise a ValueError exception
+        with the message size must be >= 0
+
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
