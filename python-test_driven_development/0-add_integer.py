67646905779a09f0da1de8a77fd9256d5114ef43
HolbertonGab <7290@holbertonstudents.com>
2024-02-07 16:48:21 +0000

    fix on syntax

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
+"""
+module add two integers
+functions:
+    add_integer: add two integers
+"""
+
+
 def add_integer(a, b=98):
-    """Adds two integers.
+    """add two integers
+    Args:  a: first integer  b: second integer
+    Returns: the return value. a + b"""
 
-    Args:
-        a (int or float): The first number.
-        b (int or float, optional): The second number. Defaults to 98.
-
-    Returns:
-        int: The sum of a and b, after casting them to integers if necessary.
-
-    Raises:
-        TypeError: If a or b is not an integer or float.
-    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+
+    return int(a + b)
