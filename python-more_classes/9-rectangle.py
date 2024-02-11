c2c7e82b7070a53ea3c63e47b2fd5e4852990e82
HolbertonGab <7290@holbertonstudents.com>
2024-02-11 10:28:06 +0000

    inverse 2 lines in def __init__ part

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -41,8 +41,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize the Rectangle instance with width and height."""
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     def __str__(self):
