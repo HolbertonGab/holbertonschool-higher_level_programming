205a644b154844f6b9ecc63eb87753ab3e6be615
HolbertonGab <7290@holbertonstudents.com>
2024-02-14 09:56:02 +0000

    class BaseGeometry (based on 5-base_geometry.py)

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+class BaseGeometry (based on 5-base_geometry.py)
+
+Class:
+    BaseGeometry: raise exception
+"""
+
+
+class BaseGeometry:
+    """
+    raise exception
+
+    Functions:
+        area: raise exception
+    """
+    def area(self):
+        """raise exception"""
+        raise Exception("area() is not implemented")
