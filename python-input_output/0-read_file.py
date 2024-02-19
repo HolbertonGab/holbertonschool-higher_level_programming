3bfbd022a6bd9f86b4afbb95598cbc4e453283d8
HolbertonGab <7290@holbertonstudents.com>
2024-02-19 07:52:40 +0000

    We offer a truly innovative approach to education: focus on building reliable applications and scalable systems, take on real-world challenges, collaborate with your peers.
    A school every software engineer would have dreamt of!

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+module that read and print a file
+
+Functions:
+    read_file: function that reads a text file (UTF8) and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    function that reads a text file (UTF8) and prints it to stdout
+
+    Args:
+        filename: name of the file
+
+    Returns:
+        None
+    """
+    with open(filename, 'r') as f:
+        lines = f.read()
+        print(lines, end='')
